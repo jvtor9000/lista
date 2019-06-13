514688d185f711523e42837250b15a388ef3a6be
jvtor9000 <51447118+jvtor9000@users.noreply.github.com>
2019-06-13 08:54:32 -0300

    Add files via upload

---
+++ lis6.c
@@ -0,0 +1,38 @@
+#include<stdlib.h>
+
+
+int main()
+{
+ int andar = 0;
+        int aux[50],i,f,g,a;
+        andar = 8;
+
+        for (i = 1; i <= andar; i++) {
+
+            int pascal[i];
+
+            for (g = andar; g > i; g--) {
+                printf(" ");
+            }
+
+            for (f = 0; f < i; f++) {
+
+                if (f == 0 || f == (i - 1)) {
+                    pascal[j] = 1;
+                } else {
+                    pascal[f] = aux[f] + aux[f - 1];
+                }
+
+                printf("%d  ",pascal[f] );
+            }
+             for (a = 0; a < f; a++) {
+                 aux[a] = pascal[a];
+
+             }
+
+
+
+            printf("\n");
+        }
+}
+
